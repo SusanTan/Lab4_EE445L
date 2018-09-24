3825c0d94d95b9985449673bb39a82dbaee827af
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2018-09-24 11:36:28 -0500

    remove build files

--- portf.d
+++
@@ -1,4 +0,0 @@
-.\portf.o: PortF.c
-.\portf.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\portf.o: tm4c123gh6pm.h
-.\portf.o: PortF.h
--- sounddriver.d
+++
@@ -1,4 +0,0 @@
-.\sounddriver.o: SoundDriver.c
-.\sounddriver.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\sounddriver.o: tm4c123gh6pm.h
-.\sounddriver.o: PWM.h
